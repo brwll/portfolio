b2b13b0e131a0ac52bf889a03ca94d0ffb16989e
brwll <rossmichaelburwell@gmail.com>
2019-08-03 16:24:03 -0700

    new version

--- js/main.js
+++ js/main.js
@@ -1,16 +1,56 @@
+$(document).ready(function() {
+  $(".element").throwable({
+    drag: true,
+    gravity: {
+      x: 0,
+      y: 0
+    },
+    impulse: {
+      f: 500,
+      p: {
+        x: 1,
+        y: 5
+      }
+    },
+    autostart: true,
+    bounce: 0.5,
+    damping: 50
+  });
+});
 
-var Main = (function ($) {
-	return {
-		myFunction: function () {
-      // Do the stuff here
-		},
-		initMain: function () {
-			$(document).ready(function () {
-				Main.myFunction();
-			})
-		}
-	};
-// Pass in jQuery.
-})(jQuery);
 
-Main.initMain();
+
+$('.element').on('mouseenter', function() {
+  var projectId = $(this).attr('data-project-id');
+  $('.image[data-project-id="'+projectId+'"]').addClass("show");
+}).on('mouseleave', function() {
+  var projectId = $(this).attr('data-project-id');
+  $('.image[data-project-id="'+projectId+'"]').removeClass("show");
+});
+
+$('.element').on('mouseenter', function() {
+  var projectId = $(this).attr('data-project-id');
+  $('.hoverimage[data-project-id="'+projectId+'"]').addClass("show");
+}).on('mouseleave', function() {
+  var projectId = $(this).attr('data-project-id');
+  $('.hoverimage[data-project-id="'+projectId+'"]').removeClass("show");
+});
+
+
+    $(function(){
+	$(".menu").click(function(){
+  	$(".menuoverlay").toggleClass("open");
+  });
+});
+
+$(function(){
+	$(".menu").click(function(){
+  	$(".menu").toggleClass("open");
+  });
+});
+
+    $(function(){
+	$(".menu").click(function(){
+  	$(".element").toggleClass("elementoverlay");
+  });
+});
